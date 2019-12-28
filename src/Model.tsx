44fb5abe2bf881d8906652438d85b05a2aab5c4c
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-12-28 23:24:38 +0900

    add

--- src/Model.tsx
+++ src/Model.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 
 export const Model = () => {
-  return     <mesh>
-      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+  return  <mesh>
+      <boxGeometry attach="geometry" args={[1, 1, 1]} />
       <meshNormalMaterial attach="material" />
     </mesh>
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
-import React, { useRef } from "react"
+import React, { useRef, useEffect } from "react"
 import { render } from "react-dom"
 import { Canvas, useFrame,useThree } from 'react-three-fiber'
-import { Camera } from "three"
+import {  Camera } from "three"
 import { Model } from "./Model"
-
-
+import * as STLExporter from "./STLExporter"
 
 function Thing() {
   const ref = useRef()
@@ -19,17 +18,33 @@ function Thing() {
   // camera.far = 1000;
 
   // camera.up.set(0, 0, 1);
-  camera.position.set(2,2,5);
-  console.log(camera.position)
+  camera.position.set(2, 2, 5);
+  const { scene } = useThree();
+  console.log("a")
+  useEffect(() => {
+
+    const s = STLExporter.parse(scene)
+    console.log(s)
+  },[scene])
   return (
     <Model/>
   )
 }
 
+const Export = () => {
+  return <button onClick={() => {
+    // const s = STLExporter.parse(scene)
+    // console.log(s)
+  }}>Exp</button>
+}
+
 const App = () => {
-  return <Canvas >
-    <Thing></Thing>
-  </Canvas>
+  return <div>
+    <Export/>
+    <Canvas >
+      <Thing></Thing>
+    </Canvas>
+  </div>
 }
 
 render(<App />, document.querySelector("#root"))
