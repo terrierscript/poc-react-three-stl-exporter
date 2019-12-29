1a356496912ae5873efab3463f34b10b9128cbf7
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-12-29 20:22:27 +0900

    add

--- src/Model.tsx
+++ src/Model.tsx
@@ -4,8 +4,8 @@ import React from "react"
 export const Model = () => {
   return (
     <mesh position={[0, 0, 0]}>
+      {/* <cylinderBufferGeometry attach="geometry" args={[0.5, 0.5, 0.5]} /> */}
       <cylinderGeometry attach="geometry" args={[0.5, 0.5, 0.5]} />
-      {/* <cylinderGeometry attach="geometry" args={[0.5, 0.5, 0.5]} /> */}
       <meshBasicMaterial attach="material" />
     </mesh>
   )
@@ -13,8 +13,8 @@ export const Model = () => {
 export const Model2 = () => {
   return (
     <mesh position={[0.5, 0.2, 0]}>
-      <boxBufferGeometry attach="geometry" args={[1, 0.2, 0.5]} />
-      {/* <boxGeometry name="box2" attach="geometry" args={[1, 0.2, 0.5]} /> */}
+      {/* <boxBufferGeometry attach="geometry" args={[1, 0.2, 0.5]} /> */}
+      <boxGeometry attach="geometry" args={[1, 0.2, 0.5]} />
       <meshNormalMaterial attach="material" />
     </mesh>
   )
--- src/exports/toRenderble.tsx
+++ src/exports/toRenderble.tsx
@@ -5,20 +5,49 @@ import {
   Geometry,
   Float32BufferAttribute,
   BufferGeometry,
-  Object3D
+  Object3D,
+  BufferAttribute
 } from "three"
+import { BufferGeometryUtils } from "three/examples/jsm/utils/BufferGeometryUtils"
 
 export const isMesh = (obj: Object3D): obj is Mesh => {
   //@ts-ignore
   return obj.isMesh
 }
 export const isBufferGeometry = (obj: any): obj is BufferGeometry => {
-  //@ts-ignore
   return obj.isBufferGeometry
+}
+export const isGeometry = (obj: any): obj is Geometry => {
+  return obj.isGeometry
+}
+
+const toBufferGeometry = (geom: Geometry | BufferGeometry): Geometry | null => {
+  console.log(geom)
+  if (isGeometry(geom)) {
+    return geom
+  }
+  // const buf = new BufferGeometry().fromGeometry(geom.clone())
+  // const c = geom.clone()
+
+  // console.log(geom)
+  // console.log(c)
+  // c.setAttribute(
+  //   "position",
+  //   //   new BufferAttribute(new Float32Array([0, 0, 0, 0, 0, 0, 0, 0, 0]), 3)
+  //   [],
+  //   3
+  // )
+  // console.log("===")
+  // console.log(c.)
+  // c.computeVertexNormals()
+  // c.merge
+  // const b = new Geometry().fromBufferGeometry(c)
+  return null
 }
 
 export const toRenderble = (scene: Scene): Scene => {
-  const geometry = new Geometry()
+  let tmpGeometry = new Geometry()
+  // let baseMesh = new Mesh()
   const copyScene = scene.clone()
 
   copyScene.traverse((obj) => {
@@ -27,24 +56,19 @@ export const toRenderble = (scene: Scene): Scene => {
     if (!mesh.geometry) {
       return
     }
-    obj.updateMatrix()
+
     mesh.material = new MeshBasicMaterial()
     // @ts-ignore
-    const geom = obj.geometry.clone()
-    if (isBufferGeometry(geom)) {
-      console.log(obj.geometry)
-      // geom.setAttribute("position", new Float32BufferAttribute([0, 0, 0], 3))
-      const convertedGeometry = new Geometry().fromBufferGeometry(geom) //.obj.geometry)
-      mesh.geometry = convertedGeometry
-    } else {
-      mesh.geometry = geom
+    const appendGeom = toBufferGeometry(mesh.geometry)
+    if (!appendGeom) {
+      return null
+      // mesh.geometry = appendGeom
     }
-    geometry.mergeMesh(mesh)
+    tmpGeometry.mergeMesh(mesh)
   })
 
   const outputScene = new Scene()
-  const buf = new BufferGeometry().fromGeometry(geometry)
-  const mesh = new Mesh(buf, new MeshBasicMaterial())
+  const mesh = new Mesh(tmpGeometry, new MeshBasicMaterial())
   outputScene.add(mesh)
   return outputScene
 }
