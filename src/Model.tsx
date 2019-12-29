20124d175da80ac99f95314f0198aeab2c4d93ca
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-12-29 21:29:57 +0900

    add

--- src/Model.tsx
+++ src/Model.tsx
@@ -11,20 +11,20 @@ export const Model = () => {
 }
 const Model1 = () => {
   return (
-    <mesh position={[0, 0.1, 0]}>
-      <cylinderBufferGeometry attach="geometry" args={[5,5,5]} />
-      <meshNormalMaterial attach="material"/>
+    <mesh position={[0, 0, 0]}>
+      <cylinderBufferGeometry attach="geometry" args={[5, 5, 5]} />
+      <meshNormalMaterial attach="material" />
     </mesh>
   )
 }
 const Model2 = () => {
   return (
     <mesh>
-      <mesh position={[5, 0, 0]}>
-        <boxBufferGeometry attach="geometry" args={[10, 2, 5]} />
-        <meshNormalMaterial attach="material"/>
+      <mesh position={[5, -1.5, 3]}>
+        <boxBufferGeometry attach="geometry" args={[6, 2, 5]} />
+        <meshNormalMaterial attach="material" />
       </mesh>
-      <mesh position={[0, 5, 0]}>
+      <mesh position={[0, 3, -1]}>
         <octahedronBufferGeometry attach="geometry" args={[4]} />
         <meshNormalMaterial attach="material" />
       </mesh>
